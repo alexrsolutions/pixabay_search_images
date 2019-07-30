dae8ef6e7a1ff91473fdee2cb01d232db78b7897
alexrsolutions <salvador.alex.ramirez@gmail.com>
2019-07-29 22:51:58 -0500

    Updates

---
+++ precache-manifest.fd2643ce322641716d0da63ec584d7f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1810c4a1589ab308a10a63e0d56fb247",
+    "url": "/pixabay_search_images/index.html"
+  },
+  {
+    "revision": "e7885789090b6da855e6",
+    "url": "/pixabay_search_images/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "48f33b7a8461a2427bda",
+    "url": "/pixabay_search_images/static/js/2.919e9873.chunk.js"
+  },
+  {
+    "revision": "e7885789090b6da855e6",
+    "url": "/pixabay_search_images/static/js/main.1a95ae94.chunk.js"
+  },
+  {
+    "revision": "2a625f6fa54be1197ceb",
+    "url": "/pixabay_search_images/static/js/runtime~main.25f6f4a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac60daca4f4f98865a43389291cd7a5e.js"
+  "/pixabay_search_images/precache-manifest.fd2643ce322641716d0da63ec584d7f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixabay_search_images/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
